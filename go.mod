5e5db9aef515407bea260357cce19da9fc7573d9
Mark Hambrecht <mhambrecht@phoenis.de>
2023-11-09 17:16:26 +0100

    Finished aplusContent

--- go.mod
+++ go.mod
@@ -1,12 +1,12 @@
 module github.com/phoenis-maha/amazon-sp-api-sdk
 
-go 1.21.1
+go 1.21.3
 
 require github.com/basgys/goxml2json v1.1.0
 
 require (
 	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- resources/aplusContent.go
+++ resources/aplusContent.go
@@ -37,9 +37,109 @@ func GetContentDocument(params *SellingPartnerParams) error {
 	if present := params.Query.Has("includedDataSet"); !present {
 		return fmt.Errorf("query param 'includedDataSet' not present")
 	}
-
 	params.Method = "GET"
 	params.APIPath = "/aplus/2020-11-01/contentDocuments"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s", params.PathParams["contentReferenceKey"])
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func UpdateContentDocument(params *SellingPartnerParams) error {
+	if present := params.Query.Has("contentReferenceKey"); !present {
+		return fmt.Errorf("query param 'contentReferenceKey' not present")
+	}
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+	params.Method = "POST"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s", params.PathParams["contentReferenceKey"])
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func ListContentDocumentAsinRelations(params *SellingPartnerParams) error {
+	if _, present := params.PathParams["contentReferenceKey"]; !present {
+		return fmt.Errorf("path param 'contentReferenceKey' not present")
+	}
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+	params.Method = "GET"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s/asins", params.PathParams["contentReferenceKey"])
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func PostContentDocumentAsinRelations(params *SellingPartnerParams) error {
+	if _, present := params.PathParams["contentReferenceKey"]; !present {
+		return fmt.Errorf("path param 'contentReferenceKey' not present")
+	}
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+
+	params.Method = "POST"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s/asins", params.PathParams["contentReferenceKey"])
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func ValidateContentDocumentAsinRelations(params *SellingPartnerParams) error {
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+
+	params.Method = "POST"
+	params.APIPath = "/aplus/2020-11-01/contentAsinValidations"
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func SearchContentPublishRecords(params *SellingPartnerParams) error {
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+	if present := params.Query.Has("asin"); !present {
+		return fmt.Errorf("query param 'asin' not present")
+	}
+
+	params.Method = "GET"
+	params.APIPath = "/aplus/2020-11-01/contentPublishRecords"
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func PostContentDocumentApprovalSubmission(params *SellingPartnerParams) error {
+	if _, present := params.PathParams["contentReferenceKey"]; !present {
+		return fmt.Errorf("path param 'contentReferenceKey' not present")
+	}
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+
+	params.Method = "POST"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s/approvalSubmissions", params.PathParams["contentReferenceKey"])
+	params.RestoreRate = 100 * time.Millisecond
+
+	return nil
+}
+
+func PostContentDocumentSuspendSubmission(params *SellingPartnerParams) error {
+	if _, present := params.PathParams["contentReferenceKey"]; !present {
+		return fmt.Errorf("path param 'contentReferenceKey' not present")
+	}
+	if present := params.Query.Has("marketplaceId"); !present {
+		return fmt.Errorf("query param 'marketplaceId' not present")
+	}
+
+	params.Method = "POST"
+	params.APIPath = fmt.Sprintf("/aplus/2020-11-01/contentDocuments/%s/suspendSubmissions", params.PathParams["contentReferenceKey"])
 	params.RestoreRate = 100 * time.Millisecond
 
 	return nil
